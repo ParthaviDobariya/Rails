0bdd6d4ac097db8f84b218ed04880785e03d3e12
ParthaviDobariya <parthavi.dobariya@softices.com>
2018-08-07 16:01:58 +0530

    activity #4203 implement search and filters

--- library/app/controllers/home_pages_controller.rb
+++ library/app/controllers/home_pages_controller.rb
@@ -1,8 +1,8 @@
 class HomePagesController < ApplicationController
   
   def index
-  	if params[:title]
-      @books = Book.where("title LIKE :name OR isbn_no LIKE :name OR YEAR(publish_date) LIKE :name OR pages LIKE :name", name: "%#{params[:title]}%").order(:title).page(params[:page]).per(3)
+  	if params[:title].present? || params[:isbn_no].present? || params[:year].present? || params[:author].present? || params[:publication_id].present? || params[:category_id].present? 
+      @books = Book.joins(:authors, :categories).where("title LIKE ? AND isbn_no LIKE ? AND YEAR(publish_date) LIKE ? AND publication_id LIKE ? AND authors.name LIKE ? AND books_categories.category_id LIKE ?", "%#{params[:title]}%", "%#{params[:isbn_no]}%", "%#{params[:year]}%", "%#{params[:publication_id]}%", "%#{params[:author]}%", "%#{params[:category_id]}%").order(:title).page(params[:page]).per(3)
     else
       @books = Book.order(:title).page(params[:page]).per(3)
     end
@@ -11,7 +11,7 @@ class HomePagesController < ApplicationController
 	private
     # Never trust parameters from the scary internet, only allow the white list through.
     def home_page_params
-      params.require(:home_page).permit(:title, :selected)
+      params.require(:home_page).permit(:title, :isbn_no, :year, :publication_id,:author, :category_id, :selected)
     end
 
 end
