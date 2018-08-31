4c2e617b50091f52ee5c99ac572c59eb87ec3c5c
ParthaviDobariya <parthavi.dobariya@softices.com>
2018-08-31 16:51:38 +0530

    activity #4258 after signup work flow

--- library/app/models/user.rb
+++ library/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, presence: true, uniqueness: true
-	validates :mobile_no, presence: true, length: { minimum:10 }, numericality: true, uniqueness: true
+	validates :mobile_no, presence: true, length: { minimum:10, maximum:13 }, numericality: true, uniqueness: true
 	validates :name,  	  presence: true
 	validates :address,   presence: true
 
