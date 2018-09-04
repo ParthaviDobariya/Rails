b8f4afdd8f73c9dd34beccd6b995013c8249083c
ParthaviDobariya <parthavi.dobariya@softices.com>
2018-09-04 10:44:07 +0530

    some changes in search and filter using build condition

--- library/app/controllers/home_pages_controller.rb
+++ library/app/controllers/home_pages_controller.rb
@@ -1,12 +1,13 @@
 class HomePagesController < ApplicationController
   def index
     conditions = {}
-    conditions[:publish_date]                   = params[:year]           if params[:year].present?
     conditions[:isbn_no]                        = params[:isbn_no]        if params[:isbn_no].present?
     conditions[:id]                             = params[:book_id]        if params[:book_id].present?
     conditions[:publication_id]                 = params[:publication_id] if params[:publication_id].present?
     conditions[:"books_categories.category_id"] = params[:category_id]    if params[:category_id].present?
-    @books = Book.joins(:categories).where(conditions).order(:title).page(params[:page]).per(3)
+    conditions[:"books_authors.author_id"]      = params[:author_id]      if params[:author_id].present?
+    @books = Book.joins(:categories,:authors).where(conditions).order(:title).page(params[:page]).per(3)
+    @books = @books.where("YEAR(publish_date) LIKE ?","#{params[:year]}") if params[:year].present?
     @year           = params[:year]
     @publication_id = params[:publication_id]
     @category       = params[:category_id]
